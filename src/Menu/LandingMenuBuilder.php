c5b57747dbee1d7510473417970bcd3ed39e87e8
Tac Tacelosky <tac@survos.com>
2019-06-18 14:39:24 -0400

    drop fosuser

--- src/Menu/LandingMenuBuilder.php
+++ src/Menu/LandingMenuBuilder.php
@@ -34,12 +34,72 @@ class LandingMenuBuilder
 
     public function createTestMenu(array $options)
     {
+
         $menu = $this->factory->createItem('root');
+        $menu->addChild(
+            'linking',
+            [
+                'route' => 'profile',
+            ]
+        );
+
+        $menu->addChild(
+            'texting',
+            [
+                'labelAttributes' => [
+                    'class' => 'class3 class4',
+                ],
+            ]
+        );
+
+        $dropdown = $menu->addChild(
+            'Hello Me',
+            [
+                'attributes' => [
+                    'dropdown' => true,
+                ],
+            ]
+        );
+
+        $dropdown->addChild(
+            'Profile',
+            [
+                'route' => 'profile',
+                'attributes' => [
+                    'divider_append' => true,
+                ],
+            ]
+        );
+
+        $dropdown->addChild(
+            'text',
+            [
+                'attributes' => [
+                    'icon' => 'fa fa-user-circle',
+                ],
+                'labelAttributes' => [
+                   // 'class' => ['class1', 'class2'],
+                ],
+            ]
+        );
+
+        $dropdown->addChild(
+            'Logout',
+            [
+                'route' => 'logout',
+                'attributes' => [
+                    'divider_prepend' => true,
+                    'icon' => 'fa fa-sign-out',
+                ],
+            ]
+        );
+        /*
         $menu->setChildrenAttribute('class', 'nav navbar-nav');
         $menu->addChild('Projects', array('uri' => '#acme_hello_projects'))
             ->setAttribute('icon', 'fa fa-list');
         $menu->addChild('Employees', array('uri' => '#acme_hello_employees'))
             ->setAttribute('icon', 'fa fa-group');
+        */
         return $menu;
     }
 }
