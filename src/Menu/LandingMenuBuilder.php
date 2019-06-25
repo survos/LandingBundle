759516cae377891727b64d463a338e42b9cbcfe0
tac <tacman@gmail.com>
2019-06-25 11:16:21 -0400

    update to bs4

--- src/Menu/LandingMenuBuilder.php
+++ src/Menu/LandingMenuBuilder.php
@@ -3,33 +3,104 @@
 namespace Survos\LandingBundle\Menu;
 
 use Knp\Menu\FactoryInterface;
+use Knp\Menu\ItemInterface;
+use Knp\Menu\MenuItem;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 class LandingMenuBuilder
 {
     private $factory;
+    private $authorizationChecker;
 
     /**
      * @param FactoryInterface $factory
      *
      * Add any other dependency you need
      */
-    public function __construct(FactoryInterface $factory)
+    public function __construct(FactoryInterface $factory, AuthorizationCheckerInterface $authorizationChecker)
     {
         $this->factory = $factory;
+        $this->authorizationChecker = $authorizationChecker;
     }
 
     public function createMainMenu(array $options)
     {
         $menu = $this->factory->createItem('root');
 
-        $menu->setChildrenAttribute('class', 'nav navbar-nav');
+        $menu->setChildrenAttribute('class', 'nav navbar-nav mr-auto');
         $menu->addChild('Home', ['route' => 'survos_landing'])
             ->setAttribute('icon', 'fa fa-home');
         $menu->addChild('xx', ['route' => 'survos_landing']);
         $menu->addChild('Three', ['route' => 'survos_landing']);
         // ... add more children
 
-        return $menu;
+        return $this->cleanupMenu($menu);
+    }
+
+    public function createAuthMenu(array $options)
+    {
+        $menu = $this->factory->createItem('root');
+        // hack?  Seems like this should be in the renderer.  Top Level ul tag
+        $menu->setChildrenAttribute('class', 'navbar-nav mr-auto');
+        $dropdown = $menu->addChild(
+            'my_account',
+            [
+                'attributes' => [
+                    'dropdown' => true,
+                ],
+            ]
+        );
+
+        if ($this->authorizationChecker->isGranted('ROLE_USER')) {
+
+
+        $dropdown->addChild(
+            'Profile',
+            [
+                'route' => 'profile',
+                'attributes' => [
+                    'icon' => 'fa fa-user-circle',
+                    'divider_append' => true,
+                ],
+            ]
+        );
+
+        $dropdown->addChild(
+            'Logout',
+            [
+                'route' => 'logout',
+                'attributes' => [
+                    'divider_prepend' => true,
+                    'icon' => 'fas fa-sign-out-alt',
+                ],
+            ]
+        );
+    } else {
+            $dropdown->addChild(
+                'Login',
+                [
+                    'route' => 'login',
+                    'attributes' => [
+                        'divider_prepend' => true,
+                        'icon' => 'fas fa-sign-in-alt',
+                    ],
+                ]
+            );
+
+            $dropdown->addChild(
+                'Register',
+                [
+                    'route' => 'register',
+                    'attributes' => [
+                        'icon' => 'fas fa-user',
+                    ],
+                ]
+            );
+
+        }
+
+        return $this->cleanupMenu($menu);
+
     }
 
     public function createTestMenu(array $options)
@@ -37,7 +108,7 @@ class LandingMenuBuilder
 
         $menu = $this->factory->createItem('root');
         // hack?  Seems like this should be in the renderer.  Top Level ul tag
-        $menu->setChildrenAttribute('class', 'nav navbar-nav');
+        $menu->setChildrenAttribute('class', 'navbar-nav mr-auto');
 
         $menu->addChild(
             'linking',
@@ -92,7 +163,7 @@ class LandingMenuBuilder
                 'route' => 'logout',
                 'attributes' => [
                     'divider_prepend' => true,
-                    'icon' => 'fa fa-sign-out',
+                    'icon' => 'fas fa-sign-out-alt',
                 ],
             ]
         );
@@ -104,13 +175,19 @@ class LandingMenuBuilder
             ->setAttribute('icon', 'fa fa-group');
         */
 
+        return $this->cleanupMenu($menu);
+        // return $menu;
+    }
+
+    private function cleanupMenu(ItemInterface $menu): ItemInterface
+    {
+
         $menu->setChildrenAttribute('class', 'navbar-nav');
 // menu items
         foreach ($menu as $child) {
             $child->setLinkAttribute('class', 'nav-link')
                 ->setAttribute('class', 'nav-item');
         }
-
         return $menu;
     }
 }
