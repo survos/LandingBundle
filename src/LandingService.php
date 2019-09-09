e26d3281d449cdc8306713d21ce60404f3e50867
Tac Tacelosky <tacman@gmail.com>
2019-09-09 15:49:11 -0400

    remove unused code

--- src/LandingService.php
+++ src/LandingService.php
@@ -5,16 +5,13 @@ namespace Survos\LandingBundle;
 class LandingService
 {
     private $entityClasses;
-    private $configManager;
     public function __construct(array $entityClasses)
     {
         $this->entityClasses = $entityClasses;
-        // dump($entityClasses); die("Stopped");
     }
 
     public function getEntities()
     {
-        // return $this->configManager->getBackendConfig();
         return $this->entityClasses;
     }
 
