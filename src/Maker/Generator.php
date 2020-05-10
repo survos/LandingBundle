a42b480932c28bc08b62e3420884a9ab030bbe47
Tac Tacelosky <tacman@gmail.com>
2020-05-10 11:52:33 -0400

    fix path to templates

--- src/Maker/Generator.php
+++ src/Maker/Generator.php
@@ -1,10 +1,11 @@
 <?php
 
-
 namespace Survos\LandingBundle\Maker;
 
+use Symfony\Bundle\MakerBundle\Generator as SymfonyGenerator;
 
-class Generator
+class Generator extends \Symfony\Bundle\MakerBundle\Generator
 {
+
 
 }
